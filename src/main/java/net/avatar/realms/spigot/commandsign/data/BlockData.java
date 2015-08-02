88eb8af36769f7ef8f59a43bc5a412b8700a32ca
Noko <nokorbis@gmail.com>
2015-08-03 01:41:25 +0200

    Create transform methodsg

--- src/main/java/net/avatar/realms/spigot/commandsign/data/BlockData.java
+++ src/main/java/net/avatar/realms/spigot/commandsign/data/BlockData.java
@@ -1,8 +1,17 @@
 package net.avatar.realms.spigot.commandsign.data;
 
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+
+import net.avatar.realms.spigot.commandsign.CommandSign;
 
 public class BlockData {
+	
+	private String worldUuid;
 	
 	private int x;
 	private int y;
@@ -46,6 +55,38 @@ public class BlockData {
 		this.type = type;
 	}
 	
+	public String getWorldUuid() {
+		return worldUuid;
+	}
+
+	public void setWorldUuid(String worlduuid) {
+		this.worldUuid = worlduuid;
+	}
 	
+	public static BlockData transform (Block block) {
+		BlockData data = new BlockData();
+		
+		data.setWorldUuid(block.getWorld().getUID().toString());
+		
+		data.setX(block.getX());
+		data.setY(block.getY());
+		data.setZ(block.getZ());
+		
+		data.setType(block.getType());
+		return data;
+	}
+	
+	public static Block transform (BlockData data) {
+		Block block = null;
+		
+		Location loc = new Location(Bukkit.getWorld(UUID.fromString(data.getWorldUuid())), data.getX(), data.getY(), data.getZ());
+		block = loc.getBlock();
+		
+		if (!block.getType().equals(data.getType())) {
+			CommandSign.getPlugin().getLogger().warning("A Command sign has been loaded at " + block.getX() + ":"+ block.getY()+":" + block.getZ()+" with a wrong type.");
+		}
+		
+		return block;
+	}
 
 }
--- src/main/java/net/avatar/realms/spigot/commandsign/data/CommandBlockData.java
+++ src/main/java/net/avatar/realms/spigot/commandsign/data/CommandBlockData.java
@@ -2,6 +2,8 @@ package net.avatar.realms.spigot.commandsign.data;
 
 import java.util.List;
 
+import net.avatar.realms.spigot.commandsign.model.CommandBlock;
+
 public class CommandBlockData {
 
 	private BlockData block;
@@ -44,6 +46,37 @@ public class CommandBlockData {
 
 	public void setNeededPermissions(List<String> neededPermissions) {
 		this.neededPermissions = neededPermissions;
+	}
+	
+	public static CommandBlockData transform (CommandBlock cmd) {
+		CommandBlockData data = new CommandBlockData();
+		
+		data.setBlock(BlockData.transform(cmd.getBlock()));
+		data.setCommands(cmd.getCommands());
+		data.setPermissions(cmd.getPermissions());
+		data.setNeededPermissions(cmd.getNeededPermissions());
+		
+		return data;
+	}
+	
+	public static CommandBlock transform (CommandBlockData data) {
+		CommandBlock cmd = new CommandBlock();
+		
+		cmd.setBlock(BlockData.transform(data.getBlock()));
+		
+		for (String str : data.getCommands()) {
+			cmd.addCommand(str);
+		}
+		
+		for (String str : data.getPermissions()) {
+			cmd.addPermission(str);
+		}
+		
+		for (String str : data.getNeededPermissions()) {
+			cmd.addNeededPermission(str);
+		}
+		
+		return cmd;
 	}
 	
 	
