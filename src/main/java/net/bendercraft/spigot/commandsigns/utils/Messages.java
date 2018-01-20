3ddfa6b825b845d5a84bf14e8538435d729c725f
Nokorbis <nokorbis@gmail.com>
2018-01-20 02:15:47 +0100

    Use standard charset instead of self loaded one

--- src/main/java/net/bendercraft/spigot/commandsigns/utils/Messages.java
+++ src/main/java/net/bendercraft/spigot/commandsigns/utils/Messages.java
@@ -3,61 +3,72 @@ package net.bendercraft.spigot.commandsigns.utils;
 import net.bendercraft.spigot.commandsigns.CommandSignsPlugin;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
-public abstract class Messages {
-
-	private static final String CHARSET = "UTF-8";
+public abstract class Messages
+{
 	private static final String FILENAME = "messages.properties";
 
 	private static Properties defaultMessages = null;
 	private static Properties messages = null;
 
-	public static String get(String key) {
-		if (defaultMessages == null && messages == null) {
+	public static String get(String key)
+	{
+		if (defaultMessages == null && messages == null)
+		{
 			loadMessages();
 		}
-		try {
+		try
+		{
 			return parseColor(messages.getProperty(key));
 		}
-		catch (NullPointerException ex) {
+		catch (NullPointerException ex)
+		{
 			CommandSignsPlugin.getPlugin().getLogger().severe("A null pointer exception occurred while parsing color on key : " + key);
 			return key;
 		}
 	}
 
-	private static void loadMessages() {
+	private static void loadMessages()
+	{
 		defaultMessages = new Properties();
-
-		try {
+		try
+		{
 			InputStream in = Messages.class.getClassLoader().getResourceAsStream(FILENAME);
-			InputStreamReader reader = new InputStreamReader(in, CHARSET);
+			InputStreamReader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
 			defaultMessages.load(reader);
 			reader.close();
 		}
-		catch (IOException e) {
-			// Should never happens
+		catch (IOException e)
+		{
 		}
+
 		messages = new Properties(defaultMessages);
-		try {
+		try
+		{
 			File folder = CommandSignsPlugin.getPlugin().getDataFolder();
-			if (!folder.exists()) {
+			if (!folder.exists())
+			{
 				folder.mkdirs();
 			}
 			File custom = new File(folder, FILENAME);
-			if (custom.exists()) {
+			if (custom.exists())
+			{
 				InputStream in = new FileInputStream(custom);
-				InputStreamReader reader = new InputStreamReader(in, CHARSET);
+				InputStreamReader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
 				messages.load(reader);
 				reader.close();
 			}
-			else {
+			else
+			{
 				OutputStream out = new FileOutputStream(custom);
 				defaultMessages.store(out, "See https://github.com/Nokorbis/ar-command-signs/wiki/Custom-messages for more information about this file");
 				out.close();
 			}
 		}
-		catch (IOException ignored) {
+		catch (IOException ignored)
+		{
 		}
 	}
 
