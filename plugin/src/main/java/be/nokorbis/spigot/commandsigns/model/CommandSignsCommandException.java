7d9047c1f410c63923a399b1f63f7b6f1bf77b49
Nokorbis <nokorbis@gmail.com>
2019-09-25 18:13:40 +0200

    Fix format

--- plugin/src/main/java/be/nokorbis/spigot/commandsigns/model/CommandSignsCommandException.java
+++ plugin/src/main/java/be/nokorbis/spigot/commandsigns/model/CommandSignsCommandException.java
@@ -6,8 +6,7 @@ import be.nokorbis.spigot.commandsigns.api.exceptions.CommandSignsException;
  * Represent an exception thrown when executing a command
  * Created by nokorbis on 1/20/16.
  */
-public class CommandSignsCommandException extends CommandSignsException
-{
+public class CommandSignsCommandException extends CommandSignsException {
     public CommandSignsCommandException(String message) {
         super(message);
     }
